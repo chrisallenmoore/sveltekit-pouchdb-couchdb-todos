633885877f3cdee7486c4ecba6bffe17108b5065
Chris Allen Moore <chris@chrisallenmoore.com>
2022-01-29 19:23:33 +0700

    added ability to input todo items, get them and show them

---
+++ src/lib/TodosList.svelte
@@ -0,0 +1,56 @@
+<script>
+	export let todos = [];
+</script>
+
+<div class="mt-1 flex rounded-md shadow-sm bg-white p-2">
+	<ul class="w-full">
+		{#each todos as todo}
+			<li class="p-2">
+				<div class="flex items-center">
+					<div class="flex items-center h-6">
+						<input
+							id="todo-checkbox"
+							name="todo-checkbox"
+							type="checkbox"
+							class="focus:ring-0 h-6 w-6 text-slate-600 border-slate-300 rounded"
+						/>
+					</div>
+					<div class="ml-3 text-lg max-w-full grow">
+						<input
+							id="todo-item"
+							name="todos-item"
+							type="text"
+							class="focus:ring-0 focus:border-slate-500 block rounded-md pl-3 text-lg border-slate-100 transition w-full"
+							bind:value={todo.item}
+						/>
+					</div>
+					<div class="ml-3 text-lg items-center">
+						<button
+							type="button"
+							class="-ml-px inline-flex items-center space-x-2 px-2 py-2 border border-slate-500 text-lg font-medium rounded-md text-white bg-slate-500 hover:bg-slate-600 focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 transition"
+						>
+							<!-- https://icons.getbootstrap.com/icons/x-lg/ -->
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="20"
+								height="20"
+								fill="currentColor"
+								class="bi bi-x-lg"
+								viewBox="0 0 16 16"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"
+								/>
+								<path
+									fill-rule="evenodd"
+									d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { db } from '$lib/db.js';
+	import TodosList from '$lib/TodosList.svelte';
 
 	onMount(async () => {
 		/**
@@ -20,13 +21,68 @@
 			retry: true
 		})
 			.on('change', async function (i) {
-				await updateContact();
+				await getTodos();
 				console.log('Databases synchronized successfully');
 			})
 			.on('error', function (err) {
 				console.log('There is an error in sync' + err);
 			});
+
+		function addEventListeners() {
+			const todoInput = document.getElementById('todo-text');
+			console.log('todo-text is: ' + todoInput.value);
+			todoInput.addEventListener('keypress', newTodoKeyPressHandler, false);
+		}
+		addEventListeners();
+		await getTodos();
 	});
+
+	// Define some stuff
+	let todos = [];
+	let item = '';
+	let completed = false;
+	// Create a new todo document and enter it in the database
+	async function addTodo(event) {
+		const newTodo = {
+			_id: new Date().toISOString(),
+			item: item,
+			completed: completed
+		};
+		console.log('new todo added:');
+		console.log(newTodo);
+		/*db.put(todo, function callback(err, result) {
+			if (!err) {
+				console.log('Successfully posted a todo!');
+				showTodos();
+			}
+		});*/
+		const sendToDB = await db.post(newTodo);
+		if (sendToDB.ok) {
+			await getTodos();
+		}
+		item = '';
+	}
+
+	async function newTodoKeyPressHandler(event) {
+		const todoInput = document.getElementById('todo-text');
+		if (event.code === 'Enter') {
+			if (todoInput.value !== '') {
+				await addTodo();
+			}
+		}
+	}
+
+	/**
+	 * Show the current list of todos by reading them from the database
+	 */
+	async function getTodos() {
+		const allTodos = await db.allDocs({
+			include_docs: true
+		});
+
+		todos = allTodos.rows.map((row) => row.doc);
+		//console.log(todos);
+	}
 </script>
 
 <svelte:head>
@@ -67,14 +123,16 @@
 					id="todo-text"
 					class="focus:ring-0 focus:border-slate-500 block w-full rounded-none rounded-l-md pl-10 text-lg border-gray-300 transition placeholder:text-slate-400"
 					placeholder="What needs to be done?"
+					bind:value={item}
 				/>
 			</div>
 			<button
 				type="button"
 				class="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-slate-500 text-sm font-medium rounded-r-md text-white bg-slate-500 hover:bg-slate-600 focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 transition"
+				on:click={addTodo}
 			>
 				<!-- Heroicon name: solid/sort-ascending -->
-				<svg
+				<!--<svg
 					class="h-5 w-5 text-white"
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 20 20"
@@ -84,60 +142,14 @@
 					<path
 						d="M3 3a1 1 0 000 2h11a1 1 0 100-2H3zM3 7a1 1 0 000 2h5a1 1 0 000-2H3zM3 11a1 1 0 100 2h4a1 1 0 100-2H3zM13 16a1 1 0 102 0v-5.586l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 101.414 1.414L13 10.414V16z"
 					/>
-				</svg>
+				</svg>-->
 				<span>Add</span>
 			</button>
 		</div>
 		<div class="max-w-2xl mx-auto mt-20">
-			<div class="mt-1 flex rounded-md shadow-sm bg-white p-2">
-				<ul class="w-full">
-					<li class="p-2">
-						<div class="flex items-center">
-							<div class="flex items-center h-6">
-								<input
-									id="todo-checkbox"
-									name="todo-checkbox"
-									type="checkbox"
-									class="focus:ring-0 h-6 w-6 text-slate-600 border-slate-300 rounded"
-								/>
-							</div>
-							<div class="ml-3 text-lg max-w-full grow">
-								<input
-									id="todo-item"
-									name="todos-item"
-									type="text"
-									class="focus:ring-0 focus:border-slate-500 block rounded-md pl-3 text-lg border-slate-100 transition w-full"
-								/>
-							</div>
-							<div class="ml-3 text-lg items-center">
-								<button
-									type="button"
-									class="-ml-px inline-flex items-center space-x-2 px-4 py-3 border border-slate-500 text-lg font-medium rounded-md text-white bg-slate-500 hover:bg-slate-600 focus:outline-none focus:ring-1 focus:ring-slate-500 focus:border-slate-500 transition"
-								>
-									<!-- https://icons.getbootstrap.com/icons/x-lg/ -->
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="20"
-										height="20"
-										fill="currentColor"
-										class="bi bi-x-lg"
-										viewBox="0 0 16 16"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"
-										/>
-										<path
-											fill-rule="evenodd"
-											d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"
-										/>
-									</svg>
-								</button>
-							</div>
-						</div>
-					</li>
-				</ul>
-			</div>
+			{#if todos.length > 0}
+				<TodosList {todos} />
+			{/if}
 		</div>
 	</div>
 </div>
